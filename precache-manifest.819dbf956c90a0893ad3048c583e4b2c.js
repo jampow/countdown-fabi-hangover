5c7ca53eabe923d89fd88256ff4c80e3bb2c2dd1
Travis (from Travis CI) <no@email.com>
2019-02-08 19:45:40 +0000

    Deploy jampow/countdown-fabi-hangover to github.com/jampow/countdown-fabi-hangover.git:gh-pages

---
+++ precache-manifest.819dbf956c90a0893ad3048c583e4b2c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "697856f57709b6231816",
+    "url": "/countdown-fabi-hangover/static/js/runtime~main.697856f5.js"
+  },
+  {
+    "revision": "cfb02b2c4ad20f9e42ab",
+    "url": "/countdown-fabi-hangover/static/js/main.cfb02b2c.chunk.js"
+  },
+  {
+    "revision": "051825a582a9578baf77",
+    "url": "/countdown-fabi-hangover/static/js/1.051825a5.chunk.js"
+  },
+  {
+    "revision": "3bbbe03a7176224698e7719f3d00c10b",
+    "url": "/countdown-fabi-hangover/index.html"
+  }
+];
